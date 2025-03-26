8a1e94b0858e2f587a0ccc905de5f8aafad8c90b
ananyam-07 <ananya.m@egovernments.org>
2025-03-26 11:10:00 +0530

    Update BirthRegistrationRepository.java

--- src/main/java/digit/repository/BirthRegistrationRepository.java
+++ src/main/java/digit/repository/BirthRegistrationRepository.java
@@ -5,7 +5,6 @@ import digit.repository.rowmapper.BirthApplicationRowMapper;
 import digit.web.models.BirthApplicationSearchCriteria;
 import digit.web.models.BirthRegistrationApplication;
 import lombok.extern.slf4j.Slf4j;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -13,24 +12,32 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.List;
 
-
 @Slf4j
-@Repository
+@Repository  // Marks this class as a repository component for Spring's component scanning
 public class BirthRegistrationRepository {
 
     @Autowired
-    private BirthApplicationQueryBuilder queryBuilder;
+    private BirthApplicationQueryBuilder queryBuilder;  // Builds SQL query for birth application search
 
     @Autowired
-    private JdbcTemplate jdbcTemplate;
+    private JdbcTemplate jdbcTemplate;  // Executes SQL queries against the database
 
     @Autowired
-    private BirthApplicationRowMapper rowMapper;
+    private BirthApplicationRowMapper rowMapper;  // Maps the SQL result set to BirthRegistrationApplication objects
 
-    public List<BirthRegistrationApplication>getApplications(BirthApplicationSearchCriteria searchCriteria){
-        List<Object> preparedStmtList = new ArrayList<>();
-        String query = queryBuilder.getBirthApplicationSearchQuery(searchCriteria, preparedStmtList);
-        log.info("Final query: " + query);
+    /**
+     * Retrieves a list of birth registration applications based on search criteria.
+     * 
+     * @param searchCriteria The criteria used to filter the search
+     * @return List of BirthRegistrationApplication matching the search criteria
+     */
+    public List<BirthRegistrationApplication> getApplications(BirthApplicationSearchCriteria searchCriteria) {
+        List<Object> preparedStmtList = new ArrayList<>();  // Holds parameters for the SQL query
+        String query = queryBuilder.getBirthApplicationSearchQuery(searchCriteria, preparedStmtList);  // Build the SQL query
+        
+        log.info("Final query: " + query);  // Log the generated SQL query for debugging or auditing
+
+        // Execute the query and map the result set to a list of BirthRegistrationApplication objects
         return jdbcTemplate.query(query, preparedStmtList.toArray(), rowMapper);
     }
 }
